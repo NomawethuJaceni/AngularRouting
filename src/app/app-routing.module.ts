1c5c56de40b5b12068bcff99f72f4067dde31b43
unknown <lethukuthulantokozo@gmail.com>
2020-02-11 09:10:28 +0200

    angularRouting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AngularComponent} from './angular/angular.component';
+import {AwsComponent} from './aws/aws.component';
+import { from } from 'rxjs';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+ {path:'angular',component: AngularComponent},
+ {path:'aws',component: AwsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
