8cc0a8ec172dfd791057b3bfff46861c5a25148f
unknown <lethukuthulantokozo@gmail.com>
2020-02-12 16:20:41 +0200

    git push

--- src/app/aws/aws.component.ts
+++ src/app/aws/aws.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-aws',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aws.component.scss']
 })
 export class AwsComponent implements OnInit {
+  title = 'routing';
+  userEmails = new FormGroup({
+  email: new FormControl('',[
+  	Validators.required,
+  	Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")])
+  });  
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  get primEmail(){
+    return this.userEmails.get('email');
+    }
 }
